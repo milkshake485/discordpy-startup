c8cdf3c014f7ccc60232e9bb6536aafc2c036bc5
milkshake485 <67472248+milkshake485@users.noreply.github.com>
2020-09-23 00:20:49 +0900

    discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,11 +16,6 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
-    
-@bot.command()
-async def test(ctx):
-    await ctx.send('text')
 
-    
+
 bot.run(token)
